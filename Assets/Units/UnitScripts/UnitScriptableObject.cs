be3f14a9775a1def881fd561db1022ae5b7314d1
fatkovik <ghazariandavid@gmail.com>
2023-10-22 01:49:02 +0400

    Unit SO added

---
+++ Assets/Units/UnitScripts/UnitScriptableObject.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Unit", menuName = "UnitSO", order = 1)]
+public class UnitScriptableObject : ScriptableObject
+{
+    public int Price;
+
+    public float HealthPoints;
+    public float HealthRegenPerSecond;
+
+    public float AttackRange;
+    public float DamagePerSecond;
+
+    public float Speed;
+
+    public Sprite Icon;
+
+    public UnitType Type;
+}
