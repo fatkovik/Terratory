77439be790a0fcf78c58b8112c5714fb717e1be6
David Khudaverydan <david.khudaverdyan72@gmail.com>
2023-10-22 18:59:12 +0400

    Some sorting

---
+++ Assets/Scripts/Units/UnitConstants.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Units
+{
+    public static class UnitConstants
+    {
+        //Constants go here
+    }
+    public enum UnitType
+    {
+        Infantry = 1,
+        Tank = 2,
+        Artillery = 3
+    }
+}
