a97ed6c3d230e579f1d208d4023de66fdb3b6b6b
David Khudaverydan <david.khudaverdyan72@gmail.com>
2023-11-18 22:08:15 +0400

    Input handler wip

---
+++ Assets/Scripts/Base/Input/InputHandler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+
+namespace Base.Input
+{
+     public class InputHandler : MonoBehaviour
+    {
+        [SerializeField] private PlayerInput _playerInput;
+        [SerializeField] private Transform _transform;
+
+        private InputAction touchPositionAction;
+        private InputAction tap;
+
+        private void Awake()
+        {
+            touchPositionAction = _playerInput.actions.FindAction("TouchPosition");
+            tap = _playerInput.actions.FindAction("Tap");
+            
+        }
+
+        private void OnEnable()
+        {
+            tap.performed += Tap;
+        }
+
+        private void Tap(InputAction.CallbackContext obj)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(touchPositionAction.ReadValue<Vector2>());
+            if(Physics.Raycast(ray,out RaycastHit hit))
+            {
+                switch (obj.interaction)
+                {
+                    case TapInteraction:
+                        Debug.Log("Tap");
+                        if (hit.collider.TryGetComponent<ISelectable>(out ISelectable selectable))
+                        {
+                            selectable.Select();
+                        }
+                        break;
+                    case HoldInteraction:
+                        Debug.Log("Hold");
+                        break;
+                }
+            }
+
+            
+
+
+        }
+
+        private void TouchPosition(InputAction.CallbackContext obj)
+        {
+            var value = obj.ReadValue<Vector2>();
+            var a = Camera.main.ScreenToWorldPoint(value);
+            _transform.position = a;
+            Debug.Log(a);   
+        }
+
+        private void OnDisable()
+        {
+            tap.performed -= Tap;
+        }
+    }
+}
